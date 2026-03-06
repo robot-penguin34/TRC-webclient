a41c63c089384d09ea7c8ef072561a0c00da5289
Master Penguin <137585700+robot-penguin34@users.noreply.github.com>
2026-03-06 14:48:52 -0700

    cursed textbox done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import MessageInputBox from "./projectcomponents/MessageInputBox"
 
+async function sendMessage(message: String) {
+  console.log("sending message: " + message)
+}
+
 
 export function App() {
   return (
     <>
       <div className="fixed bottom-0 w-full p-8 self-center">
-        <MessageInputBox/>
+        <MessageInputBox onSend={sendMessage}/>
       </div>
     </>
   )
--- src/projectcomponents/MessageInputBox.tsx
+++ src/projectcomponents/MessageInputBox.tsx
@@ -1,14 +1,31 @@
 import { useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
 
-export default function MessageInputBox() {
+interface Props {
+    onSend: (message: string) => Promise<void>
+}
+
+export default function MessageInputBox(props: Props) {
     const [value, setValue] = useState("")
 
-    function handleKeyDown() {
+    async function handleKeyDown(e: React.KeyboardEvent) {
+        /* send message on enter without a shift */
+        if (!e.shiftKey && e.key === "Enter") {
+            e.preventDefault() // no newline
+            // send message and reset the buffer
+            await props.onSend(value)
 
+            // reset buffer
+            setValue("")
+        }
     }
 
     return (
-        <Textarea placeholder="Your message here sire"/>
+        <Textarea 
+            placeholder="Your message here sire"
+            value={value}
+            onKeyDown={handleKeyDown}
+            onChange={(e) => setValue(e.target.value)}
+        />
     )
 }
